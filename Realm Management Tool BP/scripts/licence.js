b5de3cce7ccf7883fc9747b6c520aa7abcecceb8
chas <karlchastin.personal@gmail.com>
2025-11-22 06:37:41 +0800

    Initial Commit

---
+++ Realm Management Tool BP/scripts/licence.js
@@ -0,0 +1,18 @@
+const ABOUT_CREATOR = `
+LICENCNE FILE
+This file is for informational purposes only and does not require any action on your part.
+Removal of this file does not affect the functionality of the addon, but we recommend keeping so players can learn more about the creator.
+
+Made by Tomhmagic Creations Ltd.
+Created for the Minecraft Marketplace.
+
+Unauthorized distribution of this addon is prohibited.
+By purchasing this addon, you directly support our efforts to create more amazing content.
+
+Want more content? Check out our other creations on the Minecraft Marketplace!
+
+Join our community on Discord for support, sneak peeks, and updates:
+https://discord.gg/tomhmagic
+`;
+
+export { ABOUT_CREATOR };
